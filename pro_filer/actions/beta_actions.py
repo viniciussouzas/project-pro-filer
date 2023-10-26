d941645c2a55b02e5248cd90734f68674f740891
Vinícius Souza <tradutorvinicius@gmail.com>
2023-10-26 17:01:17 -0300

    Resolve requisito 1

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -1,11 +1,12 @@
 """Arquivo que estudantes devem editar"""
-
+def count_slashes_file(string):
+    return string.count("/")
 
 def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
+        deepest_file = max(context["all_files"], key=count_slashes_file)
         print(f"Deepest file: {deepest_file}")
 
 
