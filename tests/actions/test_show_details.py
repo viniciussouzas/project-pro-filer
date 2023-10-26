466d8a23c270f20f990f0be2c857b8164fbf2de8
Vinícius Souza <tradutorvinicius@gmail.com>
2023-10-26 18:04:40 -0300

    fix: requisito 4

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -9,9 +9,10 @@ def test_file_exists(capsys):
   with patch('os.path.exists', Mock(return_value=True)):
     with patch('os.path.getsize', Mock(return_value=1000)):
       with patch('os.path.isdir', Mock(return_value=False)):
-        with patch('os.path.splitext', Mock(return_value=("/path/to/your/file", ".txt"))):
-           with patch('os.path.getmtime', Mock(return_value=1609459200)):
-             show_details(context)
+        with patch('os.path.splitext',
+                   Mock(return_value=("/path/to/your/file", ".txt"))):
+          with patch('os.path.getmtime', Mock(return_value=1609459200)):
+            show_details(context)
 
   captured = capsys.readouterr()
   expected = (
@@ -19,7 +20,7 @@ def test_file_exists(capsys):
     "File size in bytes: 1000\n"
     "File type: file\n"
     "File extension: .txt\n"
-    "Last modified date: 2020-12-31\n"
+    "Last modified date: 2021-01-01\n"
   )
 
   assert captured.out == expected
@@ -32,14 +33,16 @@ def test_file_does_not_exists(capsys):
     captured = capsys.readouterr()
     expected = "File 'file' does not exist\n"
     assert captured.out == expected
-  
+
 
 def test_file_with_no_extension(capsys):
     with patch('os.path.exists', Mock(return_value=True)):
         with patch('os.path.getsize', Mock(return_value=1000)):
             with patch('os.path.isdir', Mock(return_value=False)):
-                with patch('os.path.splitext', Mock(return_value=("/path/to/your/file", ""))):
-                    with patch('os.path.getmtime', Mock(return_value=1609459200)):
+                with patch('os.path.splitext',
+                           Mock(return_value=("/path/to/your/file", ""))):
+                    with patch('os.path.getmtime',
+                               Mock(return_value=1609459200)):
                         show_details(context)
 
     captured = capsys.readouterr()
@@ -48,6 +51,6 @@ def test_file_with_no_extension(capsys):
         "File size in bytes: 1000\n"
         "File type: file\n"
         "File extension: [no extension]\n"
-        "Last modified date: 2020-12-31\n"
+        "Last modified date: 2021-01-01\n"
     )
     assert captured.out == expected
